c95d2d963a2b7b2d88895e049ea786305217cec5
Brandon <brandon.l.johnson@gmail.com>
2026-05-13 15:18:03 -0500

    feat: add AP closed loop walking skeleton

--- erpnext/setup/utils.py
+++ erpnext/setup/utils.py
@@ -202,9 +202,9 @@ def enable_all_roles_and_domains():
 def _enable_all_roles_for_admin():
 	from frappe.desk.page.setup_wizard.setup_wizard import add_all_roles_to
 
-	all_roles = set(frappe.db.get_values("Role", pluck="name"))
+	all_roles = set(frappe.get_all("Role", pluck="name"))
 	admin_roles = set(
-		frappe.db.get_values("Has Role", {"parent": "Administrator"}, fieldname="role", pluck="role")
+		frappe.get_all("Has Role", filters={"parent": "Administrator"}, pluck="role")
 	)
 
 	if all_roles.difference(admin_roles):
